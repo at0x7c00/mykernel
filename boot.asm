ed1d905542a1cb2a7ae79d4f334c82cb6c6f1aac
huqiao <zoozle@qq.com>
2019-04-18 19:53:49 +0800

    use 510 - ($ - 11584) to calcute empty data space

--- boot.asm
+++ boot.asm
@@ -1,4 +1,8 @@
+org 0x7c00
+label:
+db 0,0,0
 ;es:bx指向显存
+mov ax,label
 mov ax,0xb800
 mov es,ax
 mov bx,0x00
@@ -8,7 +12,7 @@ mov bx,0x00
 ;ds:si指向字符串位置
 mov ax,0x00
 mov ds,ax
-mov ax,0x7c00+data
+mov ax,[0x7c00+data]
 mov si,ax
 
 print:
@@ -38,12 +42,12 @@ jmp $
 data:
 db 'Hello,World'
 
-times 451 db 0
+times 510 - ($ - $$) db 0
 
 db 0x55
 db 0xaa
 
 ;心得：
 ;1）寄存器太少不够用；
-;2）[0x7c00 + data + bx]中的只能用bx？
+;2）[0x7c00 + data + bx]中的只能用bx\bp\si\di四个寄存器
 ;3）0x7c00是段内偏移地址，不是段地址！
